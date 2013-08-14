f1c6c5b87cbe4b29641817b34ca5d113cb1db0a8
Derek Henscheid <derek-henscheid@uiowa.edu>
2013-08-14 07:56:31 -0500

    got all tests passing/pending, still not sure on my test design (TDD is new for me)

--- ic-transit.js
+++ ic-transit.js
@@ -1,6 +1,6 @@
 var NextBus = require('./lib/next-bus.js'),
 util = require('util');
 
-NextBus.routes('uiowa', function(routes) {
+NextBus.route('uiowa', 'blue', function(routes) {
   console.log(util.inspect(routes, {depth: 5}));
 });
--- test/test-next-bus.js
+++ test/test-next-bus.js
@@ -2,7 +2,15 @@ var should = require('should'),
     NextBus = require('../lib/next-bus');
 
 describe('NextBus', function() {
-  describe('#routes(agency, callback)', function() {
+  describe('#routes()', function() {
+    var routes;
+    before(function(done) {
+      NextBus.routes('uiowa', function(rts) {
+        routes = rts;
+        done();
+      });
+    });
+    
     it('should call callback with empty list if no routes found', function(done) {
       NextBus.routes('invalidAgency', function(routes) {
         routes.should.be.empty;
@@ -10,14 +18,60 @@ describe('NextBus', function() {
       });
     });
     
-    it('should call callback with list of objects with title and tag props', function(done) {
-      NextBus.routes('uiowa', function(routes) {
-        routes.should.not.be.empty;
+    describe('response', function() {
+      it('should not be a nonempty array', function() {
+        routes.should.be.an.instanceof(Array).and.not.be.empty;
+      });
+      
+      it('should have objects with tag and title properties', function() {
         routes.forEach(function(route) {
           route.should.have.property('title');
           route.should.have.property('tag');
-        })
+        });
+      });
+    });
+  });
+  
+  describe('#route()', function() {
+    var route;
+    before(function(done) {
+      NextBus.route('uiowa', 'red', function(rt) {
+        route = rt;
         done();
+      });
+    });
+    
+    it('should call callback with undefined if no route found', function(done) {
+      NextBus.route('invalid', 'invalid', function(invalid) {
+        should.not.exist(invalid);
+        done();
+      });
+    });
+    
+    describe('response', function() {
+      it('should have tag and title properties');
+      it('should have a non-empty array property stops');
+      it('should have a non-empty array property directions');
+      it('should have a non-empty array property paths');
+      
+      describe('stops', function() {
+        it('should have objects with tag, title and stopId properties');
+      });
+      
+      describe('directions', function() {
+        it('should have objects with non-empty array property stopTags');
+        
+        describe('stopTags', function() {
+          it('should have property tag');
+        });
+      });
+      
+      describe('paths', function() {
+        it('should have objects with non-empty array property points');
+        
+        describe('points', function() {
+          it('should have lat and lon properties');
+        });
       });
     });
   });
